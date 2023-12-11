1d8d89e747f21fa2826040980c1896be1885577a
LEO GOUBARD <goubardleo@gmail.com>
2023-12-11 21:59:54 +0100

    feat: create CatCard

---
+++ src/components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  tag: 'Animals',
+  title: 'My Cat',
+  body: 'My cat is the most beautiful one',
+  author: 'Myself',
+  time: '2h ago',
+};
+
+export const mockCatCardProps = {
+  base,
+};
